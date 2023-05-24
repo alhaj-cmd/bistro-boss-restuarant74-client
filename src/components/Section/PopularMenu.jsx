7e6e0d143ac4fbc9d0239f6ade9fa1f30cbf808d
github_username <you@domain.com>
2023-05-24 14:28:08 +0600

    json file add

---
+++ src/components/Section/PopularMenu.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+import SectionTitle from "./SectionTitle";
+
+const PopularMenu = () => {
+    return (
+        <div>
+            <SectionTitle
+            heading ="From Our Menu"
+            subHeading ="popular Itmes"
+            >
+                
+            </SectionTitle>
+        </div>
+    );
+};
+
+export default PopularMenu;
